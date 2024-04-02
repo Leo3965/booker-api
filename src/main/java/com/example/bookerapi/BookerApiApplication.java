a4f545c5a1c39a3d84083c9503c69f28d9096794
leonardo <leofreitas.engineer@gmail.com>
2024-04-02 10:48:24 -0300

    feat: starting project

---
+++ src/main/java/com/example/bookerapi/BookerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookerapi/BookerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
